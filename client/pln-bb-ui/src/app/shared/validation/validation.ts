c94f1a91d35dc7078802bd784d8f80c805543015
Adi Maulana Triadi <adimaulanat@gmail.com>
2018-11-19 00:50:23 +0700

    [ADMIN] Add validation form from add new master pltu

---
+++ client/pln-bb-ui/src/app/shared/validation/validation.ts
@@ -0,0 +1,61 @@
+declare var $:any;
+
+export function pltuValidation(){
+$('.ui.form')
+  .form({
+    fields: {
+      pltu_name: {
+        identifier: 'pltu_name',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your pltu name'
+          }
+        ]
+      },
+      pltu_code: {
+        identifier: 'pltu_code',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your pltu code'
+          }
+        ]
+      },
+      pltu_address: {
+        identifier: 'pltu_address',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your pltu address'
+            
+          }
+        ]
+      },
+      pltu_npwp: {
+        identifier: 'pltu_npwp',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your pltu npwp'
+          }
+        ]
+      },
+      pltu_status: {
+        identifier: 'pltu_status',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your pltu status'
+          }
+        ]
+      }
+    }
+  })
+;
+if ($('.ui.form').form('is valid')){
+  return true;
+} else {
+  return 0;
+}
+}
